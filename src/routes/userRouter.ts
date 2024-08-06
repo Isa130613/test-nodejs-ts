11b58208eea4c7f9281d59ff8760592427ee00ab
Isabela Ortega Vasco <isabela2603ov@gmail.com>
2024-08-05 21:46:26 -0500

    started login

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UserController from '../controllers/userController';
+import AuthController from '../controllers/authController';
 
 export const userRouter = Router();
 
 userRouter.get('/', UserController.getAllUsers);
 userRouter.post('/', UserController.createUser);
+userRouter.post('/login', AuthController.login);
 userRouter.get('/:id', UserController.getUserById);
 userRouter.put('/:id', UserController.updateUser);
 userRouter.delete('/:id', UserController.deleteUser);
